ec297c3562a8474075498e42a42e71e7a9f6fe2e
leonard-o <realcodedevs@gmail.com>
2022-03-30 18:11:22 +0300

    Modified Javascript

--- main.js
+++ main.js
@@ -5,22 +5,38 @@ const btnS = document.querySelector('#btnS');
 const span = document.querySelector('span');
 const back = document.querySelector('#back');
 
+let value = '';
+toggleDisabled(true);
+
+// toggleDisabled function added below
+
+function toggleDisabled(bool) {
+    if (bool) {
+        submit.setAttribute('disabled', 'disabled');
+    } else {
+        submit.removeAttribute('disabled');
+    }
+}
+
 btnS.addEventListener('click', (event) => {
     value = event.target.innerText;
     span.innerText = value;
+
+    // 1 line added below
+    toggleDisabled(false);
 });
 
 submit.addEventListener('click', () => {
-    if (value === '') {
-        submit.disabled = true;
-    } else {
-        submit.disabled = false;
-    }
     main.style.display = 'none';
     card.style.display = 'block';
 
     back.addEventListener('click', () => {
         card.style.display = 'none';
         main.style.display = 'block';
+
+        // 2 lines added below
+
+        value = '';
+        toggleDisabled(true);
     });
 });
